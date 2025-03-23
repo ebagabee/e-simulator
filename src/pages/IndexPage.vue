2ec346f39ae7daa0a4e6e4f6812f2ee4ad0496ec
ebagabee <ebagabe.2025@gmail.com>
2025-03-23 16:09:13 -0300

    initial setup

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,7 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <h1>Página em construção</h1>
   </q-page>
 </template>
 
-<script setup>
-//
-</script>
+<script setup></script>
